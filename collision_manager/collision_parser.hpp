a5687e2badbb21c2d59c0f00e8588e97d6e6f045
Aishwarya Murahari <aishwarya26ramagiri@gmail.com>
2025-03-13 13:39:10 -0700

    Assign ranks to the processes and partition the csv dataset among 5 processes

--- collision_manager/collision_parser.hpp
+++ collision_manager/collision_parser.hpp
@@ -10,6 +10,8 @@ class CollisionParser {
 public:
     CollisionParser(const std::string& filename);
     Collisions parse();
+    Collisions parsePartition(int start_index, int end_index);
+    int getTotalRecords();
 
 private:
     std::string filename;
